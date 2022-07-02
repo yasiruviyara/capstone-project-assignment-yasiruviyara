19bf969e6c643f5727574df4faaf9e1a5228050a
opencourse6tutor <105489421+opencourse6tutor@users.noreply.github.com>
2022-07-02 11:00:14 +0530

    Update teacher-edit.test.js

--- frontend/test/teacher-edit.test.js
+++ frontend/test/teacher-edit.test.js
@@ -5,7 +5,7 @@ fixture`Testing Teacher UI`
     .page`http://localhost:4401/`
 test('Testing edit teachers', async t => {
     await t.navigateTo("/");
-    await t.click("#teacher-edit-123456");
+    await t.click("#teacher-edit-10003");
 
     await t.typeText("#teacher-name", "Changed Teacher Name");
     await t.typeText("#teacher-age", "99");
@@ -19,5 +19,5 @@ test('Testing edit teachers', async t => {
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
     await t.expect(tdText).contains("Changed Teacher Name");
 
-    await t.click("#teacher-delete-123456");
+    await t.click("#teacher-delete-10003");
 });
